06a2f5fcad37e5a5dc3e32b592e06267c1bbd41c
diiev <ramzans096@gmail.com>
2022-05-04 17:23:22 +0300

    .

--- js/script.js
+++ js/script.js
@@ -35,19 +35,7 @@ $('.about_edu_carousel').slick({
       }
     ]
   });
-  // аккордион на js
-// let accordion_items = document.querySelectorAll('.questions_accordion');
-// for (i = 0; i <accordion_items.length; i++) {
-//     let thisItems = accordion_items[i];
-//     let nextItems=accordion_items[i+1];
-//     thisItems.addEventListener('click', function(){
-//       thisItems.classList.toggle('active'); 
-//     }) 
-//     nextItems.addEventListener('click', function () {
-//       thisItems.classList.remove('active');
-//    })
-// } 
-// аккордион на jquery
+// accordion
 $(document).ready(function(){
   $('.questions_accordion_item').click(function(event){
     $(this).toggleClass('active').next().slideToggle(400);
@@ -55,6 +43,8 @@ $(document).ready(function(){
     $('.accordion_item_content').not($(this).next()).slideUp(400);
   });
 });
+
+// burger
 $(document).ready(function(){
   $('.burger').click(function(event){
     $('.burger, .menu').toggleClass('active_burger');
@@ -62,19 +52,3 @@ $(document).ready(function(){
   });
  
   }); 
-  $(document).ready(function(){
-    $('.menu__list a').click(function(event){
-      $('.burger, .menu').remove('active_burger');
-      $('body').remove('lock') 
-    });
-   
-    }); 
- 
- const menu_burger = document.querySelector('.burger');
- const menu = document.querySelector('.menu')
- if (menu_burger.classList.contains('.active_burger')) { 
-   document.body.classList.remove('.lock');
-   menu_burger.classList.remove('.active_burger');
-   menu.classList.remove('.active_burger');
-
- }
